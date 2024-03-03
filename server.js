98236ca75278fdaa9df4cac0b5374685d032dc19
wsj <2199179969@qq.com>
2024-03-03 11:19:46 +0800

    post请求 接收urlencoded & json数据

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const { promisify }= require("util")
 const app = express()
 const readFile = promisify(fs.readFile)
 
-
+app.use(express.urlencoded())
+app.use(express.json())
 
 app.get('/', async (req, res) => {
     // fs.readFile('./db.json', 'utf-8', (err, data) => {
@@ -12,19 +13,21 @@ app.get('/', async (req, res) => {
     //         res.send(data)
     //     }
     // })
-
-
     try {
         const data = await readFile('./db.json', 'utf-8')
         res.send(data)
     } catch (error) {
         res.send(error)
     }
-    // res.send('Hello World!')
 })
-
 app.get('/hello', (req, res) => {
     res.send('Hello World! hhhhhh')
+})
+ 
+app.post('/login', (req, res) => {
+    console.log(req.headers)
+    console.log(req.body)
+    res.send('pose enter')
 })
 
 
